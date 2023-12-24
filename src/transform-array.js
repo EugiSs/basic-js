fb4387e69f5f2cae1ca90338bf6afc6be71d2832
Eugi <neskisun@gmail.com>
2023-12-25 01:29:18 +0300

    refactor: formatting

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,26 +13,6 @@ const { NotImplementedError } = require("../extensions/index.js")
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform2(arr) {
-	if (!Array.isArray(arr))
-		throw new Error("'arr' parameter must be an instance of the Array!")
-
-	let result = []
-
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] === "--discard-next") {
-			i++
-		} else if (arr[i] === "--discard-prev") {
-			if (i > 0) {
-			}
-		} else if (arr[i] === "--double-next") {
-		} else if (arr[i] === "--double-prev") {
-		} else {
-			result.push(arr[i])
-		}
-	}
-}
-
 function transform(arr) {
 	if (!Array.isArray(arr))
 		throw new Error("'arr' parameter must be an instance of the Array!")
